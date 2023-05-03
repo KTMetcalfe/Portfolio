4135449fab9d32ef1f1c5ca684e75f878d277807
Kian Metcalfe <kmetcalfe4@gmail.com>
2023-05-03 00:51:33 -0400

    Instanced cube

--- src/components/stores/CubeStore.ts
+++ src/components/stores/CubeStore.ts
@@ -1,24 +1,29 @@
 import { writable } from 'svelte/store';
 
 const createCubeStore = () => {
-	const { subscribe, set, update } = writable<
-		Set<{ x: number; y: number; z: number; color?: string }>
-	>(new Set());
+  const { subscribe, set, update } = writable<
+    Set<{ x: number; y: number; z: number; color?: string }>
+  >(new Set());
 
-	return {
-		subscribe,
-		add: (x: number, y: number, z: number, color?: string) =>
-			update((state) => {
-				state.add({ x, y, z, color});
-				return state;
-			}),
-		remove: (x: number, y: number, z: number) =>
-			update((state) => {
-				state = new Set([...state].filter((cube) => cube.x !== x || cube.y !== y || cube.z !== z));
-				return state;
-			}),
-		clear: () => set(new Set())
-	};
+  return {
+    subscribe,
+    update,
+    add: (x: number, y: number, z: number, color?: string) =>
+      update((state) => {
+        state.add({ x, y, z, color });
+        return state;
+      }),
+    remove: (x: number, y: number, z: number) =>
+      update((state) => {
+        state = new Set(
+          [...state].filter(
+            (cube) => cube.x !== x || cube.y !== y || cube.z !== z
+          )
+        );
+        return state;
+      }),
+    clear: () => set(new Set()),
+  };
 };
 
 export const CubeStore = createCubeStore();
